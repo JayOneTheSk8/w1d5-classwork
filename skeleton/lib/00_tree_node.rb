09bf5b889b47e03a29a4af0f6971c85ab99aca24
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-28 11:53:18 -0400

    Initialize PolyTreeNode

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,14 @@
 class PolyTreeNode
+  attr_reader :value, :parent
+  attr_accessor :children
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
 
+  def parent=
+  end
+
+  private
 end
