d34a8dd95b198bb3844bb096ab40133665ddd331
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-28 18:10:05 -0400

    End of day

--- tic_tac_toe/lib/tic_tac_toe_node.rb
+++ tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_accessor :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,14 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    # children = []
+    # if self.board.empty?
+    #
+    # end
+  end
+
+  def self.tictactize(array)
+    toe = []
+    
   end
 end
